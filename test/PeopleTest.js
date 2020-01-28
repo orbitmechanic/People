8252c313b6d9b4c092b1cd95fb5f14827f272ccb
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-01-27 21:54:54 -0800

    Test ownership

--- test/PeopleTest.js
+++ test/PeopleTest.js
@@ -3,80 +3,43 @@ const truffleAssert = require("truffle-assertions");
 
 contract("People", async function(accounts){
 
-  let instance;
-
-  before(async function(){
-    instance = await People.deployed()
+  it("shouldn't create a person older than 150 years.", async function(){
+    let instance = await People.new();
+    await truffleAssert.fails(instance.createPerson("Methusela", 200, 190, {value: web3.utils.toWei("1", "ether")}), truffleAssert.ErrorType.REVERT);
   });
-
-  it("shouldn't create a person with age over 150 years", async function(){
-    await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}), truffleAssert.ErrorType.REVERT);
+  it("shouldn't create a person for free.", async function(){
+    let instance = await People.deployed();
+    await truffleAssert.fails(instance.createPerson("Nobody", 50, 190, {value: 1000}), truffleAssert.ErrorType.REVERT);
   });
-  it("shouldn't create a person without payment", async function(){
-    await truffleAssert.fails(instance.createPerson("Bob", 50, 190, {value: 1000}), truffleAssert.ErrorType.REVERT);
-  });
-  it("should set senior status correctly", async function(){
-    await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether")});
+  it("should set senior status correctly.", async function(){
+    let instance = await People.deployed();
+    await instance.createPerson("Bernie", 92, 190, {value: web3.utils.toWei("1", "ether")});
     let result = await instance.getPerson();
     assert(result.senior === true, "Senior level not set");
   });
-  it("should set age correctly", async function(){
-    let result = await instance.getPerson();
-    assert(result.age.toNumber() === 65, "Age not set correctly");
-  });
-  it("should not allow non-owner to delete people", async function(){
+  it("should set age correctly.", async function(){
     let instance = await People.deployed();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-    await truffleAssert.fails(instance.deletePerson(accounts[2], {from: accounts[2]}), truffleAssert.ErrorType.REVERT);
+    let result = await instance.getPerson();
+    assert(result.age.toNumber() === 92, "Age not set correctly");
   });
-  it("should allow the owner to delete people", async function(){
+  it("should allow the contract owner to delete themselves.", async function(){
     let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("Kamakazi", 14, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
     await truffleAssert.passes(instance.deletePerson(accounts[1], {from: accounts[0]}));
   });
-  it("should add balance correctly after createPerson call with 1 ether", async function(){
+  it("should allow the contract owner to delete somebody else.", async function(){
     let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-
-    let balance = await instance.balance();
-    let floatBalance = parseFloat(balance);
-
-    let realBalance = await web3.eth.getBalance(instance.address);
-
-    assert(floatBalance == web3.utils.toWei("1", "ether") && floatBalance == realBalance)
+    await instance.createPerson("Hilliary", 76, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(instance.deletePerson(accounts[1], {from: accounts[0]}));
   });
-  it("should allow the owner to withdraw balance", async function(){
+  it("should not allow the same non-contract owner to delete themselves.", async function() {
     let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-    await truffleAssert.passes(instance.withdrawAll({from: accounts[0]}));
+    await instance.createPerson("RobinWilliams", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.fails(instance.deletePerson(accounts[2], {from: accounts[2]}), truffleAssert.ErrorType.REVERT);
   });
-  it("should not allow a non-owner to withdraw balance", async function(){
+  it("should not allow a different non-contract owner to delete a person.", async function() {
     let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-    await truffleAssert.fails(instance.withdrawAll({from: accounts[2]}), truffleAssert.ErrorType.REVERT);
+    await instance.createPerson("Hinkle", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.fails(instance.deletePerson(accounts[3], {from: accounts[2]}), truffleAssert.ErrorType.REVERT);
   });
-  it("owners balance should increase after withdrawal", async function(){
-    let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-
-    let balanceBefore = parseFloat(await web3.eth.getBalance(accounts[0]));
-    await instance.withdrawAll();
-    let balanceAfter = parseFloat(await web3.eth.getBalance(accounts[0]));
-    assert(balanceBefore < balanceAfter, "Owners balance was not increased after withdrawal");
-
-  });
-  it("should reset balance to 0 after withdrawal", async function(){
-    let instance = await People.new();
-    await instance.createPerson("Lisa", 35, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
-
-    await instance.withdrawAll();
-
-    let balance = await instance.balance();
-    let floatBalance = parseFloat(balance);
-
-    let realBalance = await web3.eth.getBalance(instance.address);
-
-    assert(floatBalance == web3.utils.toWei("0", "ether") && floatBalance == realBalance, "Contract balance was not 0 after withdrawal or did not match")
-
-  })
 });
