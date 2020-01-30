602f807797f1f3bc8af03e13357f970810d6b1c4
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2020-01-29 17:25:00 -0800

    Adding tests for value transfers

--- test/PeopleTest.js
+++ test/PeopleTest.js
@@ -25,6 +25,8 @@ contract("People", async function(accounts){
     let result = await instance.getPerson();
     assert(result.age.toNumber() === 92, "Age not set correctly");
   });
+
+
   it("should allow the contract owner to delete themselves.", async function(){
     await instance.createPerson("Kamakazi", 14, 160, {from: accounts[0], value: web3.utils.toWei("1", "ether")});
     await truffleAssert.passes(instance.deletePerson(accounts[0], {from: accounts[0]}));
@@ -42,4 +44,60 @@ contract("People", async function(accounts){
     await truffleAssert.fails(instance.deletePerson(accounts[3], {from: accounts[2]}), truffleAssert.ErrorType.REVERT);
   });
 
+
+  it("should increase balance when a person is added by owner.", async function() {
+    await instance.createPerson("Narcasist", 29, 160, {from: accounts[0], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) >= web3.utils.toWei("0.5", "ether"));
+  });
+  it("should decrease owner's balance when a person is added by owner.", async function() {
+    await instance.createPerson("Narcasist", 29, 160, {from: accounts[0], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(accounts[0]) < web3.utils.toWei("98", "ether"));
+  });
+  it("should increase balance when a person is added by a non-owner.", async function() {
+    await instance.createPerson("Shawn", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) >= web3.utils.toWei("1", "ether"));
+  });
+  it("should decrease originating balance when a person is added by a non-owner.", async function() {
+    await instance.createPerson("Sheep", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(accounts[1]) <= web3.utils.toWei("98", "ether"));
+  });
+  it("internal balance variable should match on-chain contract balance.", async function() {
+    await instance.createPerson("Smith", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    let balance = await instance.balance();
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == balance);
+  });
+  it("should increase balance by 1 Eth when a person is added.", async function() {
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == web3.utils.toWei("0", "ether"));
+    await instance.createPerson("Jones", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == web3.utils.toWei("1", "ether"));
+  });
+  it("should increase balance by 2 Eth when two people are added.", async function() {
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == web3.utils.toWei("0", "ether"));
+    await instance.createPerson("Jones", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == web3.utils.toWei("1", "ether"));
+    await instance.createPerson("JonesToo", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(web3.eth.getBalance(instance.address) == web3.utils.toWei("2", "ether"));
+  });
+  it("should allow contract owner to withdraw all of its balance.", async function() {
+    await instance.createPerson("Jones", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("JonesToo", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("OtherJones", 29, 160, {from: accounts[3], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("JJonesAnon", 29, 160, {from: accounts[4], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.passes(instance.withdrawAll({from: accounts[0]}));
+  });
+  it("should increase contract owner's balance by same amount on full withdraw.", async function() {
+    await instance.createPerson("Jones", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("JonesToo", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("OtherJones", 29, 160, {from: accounts[3], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("JJonesAnon", 29, 160, {from: accounts[4], value: web3.utils.toWei("1", "ether")});
+    await instance.withdrawAll({from: accounts[0]});
+    await truffleAssert.passes(web3.eth.getBalance(accounts[0]) >= web3.utils.toWei("103", "ether"));
+  });
+  it("should not allow non-contract owners to withdraw all of its balance.", async function() {
+    await instance.createPerson("Thief", 29, 160, {from: accounts[1], value: web3.utils.toWei("1", "ether")});
+    await instance.createPerson("Ericson", 29, 160, {from: accounts[2], value: web3.utils.toWei("1", "ether")});
+    await truffleAssert.fails(instance.withdrawAll({from: accounts[1]}));
+    await truffleAssert.fails(instance.withdrawAll({from: accounts[2]}));
+    await truffleAssert.fails(instance.withdrawAll({from: accounts[3]}));
+  });
 });
